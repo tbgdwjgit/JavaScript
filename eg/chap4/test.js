211bf71f0150a47b27c7f1579c3369ce6b6747c9
tbgdwj <17049185@qq.com>
2017-03-06 11:01:18 +0800

    add eg

---
+++ eg/chap4/test.js
@@ -0,0 +1,73 @@
+// var h = 0xe;
+// var i = 0x2;
+// var j = h*i;
+// alert(j);
+// alert("hello\t\thello\ngoodbye");
+// var mytext="dsssfdwefwsda";
+// alert(mytext.length);
+
+var myobject={
+   "name":"twx",
+   "age":"8"
+};
+
+// // alert(myobject.name);
+// if ("name" in myobject) {
+// 	alert("name property");
+// }
+
+var myDate = new Date();
+//if (myotest instanceof myobject){
+if (myDate instanceof Date){	
+	alert("myDate is mDate");
+}
+
+// var myDate = new Date();
+// //alert(myDate.getYear());
+// // alert(myDate.getFullYear());
+// // alert(myDate.toLocaleTimeString());
+// alert(myDate.getDate());
+// alert(myDate.toLocaleString());
+// alert(myDate.toDateString());
+
+// var myArr = new Array(1,2,3,"xxs","eee");
+// alert(myArr);
+// alert("Steve's response was \"Cool!\"");
+
+// alert(6%4);
+// alert("42"==42);
+// alert("34"===34);
+
+// var num=4;
+// alert(num++);
+// alert(++num);
+
+var num=4;
+var num1=5;
+var str="xxx";
+var str1="Yxd";
+alert(num+num1);
+alert(num+str);
+alert(str+str1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ eg/chap6/test.js
@@ -0,0 +1,66 @@
+//alert("123");
+// var num1=prompt("输入数字：");
+// if (num1 > 20){
+// 	alert("输入的内容：" + num1);
+// }
+
+var myArray=["ddd","eee","rrr"];
+var myArray1=["ewew","e3"];
+// for (var i = myArray.length - 1; i >= 0; i--) {
+// 	alert(myArray[i]);
+// }
+
+
+// for(var svalue in myArray){
+//     alert(svalue);
+// }
+
+//prompt("321");
+
+// var btmp= confirm("123");
+// alert(btmp);
+
+// var newarray= myArray1.concat(myArray)
+// for (var i = newarray.length - 1; i >= 0; i--) {
+// 	alert(newarray[i]);
+// }
+
+// alert(myArray.push("123"));
+// alert(myArray.join());
+// alert(myArray.pop());
+// alert(myArray.join("~"));
+
+// alert(myArray.unshift("123"));
+// alert(myArray.join());
+// alert(myArray.shift());
+// alert(myArray.join("~"));
+
+// alert(myArray.slice(0,4));
+// var myArray=["16","4","9"];
+// alert(myArray.sort());//按字母顺序
+// alert(myArray.map(Math.sqrt));
+//window.alert("全局对象");
+
+//close();
+//open();
+function sendAlert(){
+    alert("Hello");
+}
+
+function startTimer(){
+	var timerID = window.setTimeout(sendAlert,2000);
+}
+
+function checkhref(){
+var atemp=document.getElementById("aTest");
+alert(atemp.href);
+alert(atemp.innerHTML);
+atemp.innerHTML="123456";
+
+startTimer();
+}
+
+
+
+
+
